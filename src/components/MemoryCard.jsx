37d1d2edc78d2c7c26328dc2da95e3f254750e24
jamariod <j25ghs99@aol.com>
2020-03-25 12:05:18 -0400

    Updated files

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import "./MemoryCard.css";
 
 class MemoryCard extends Component {
+  constructor() {
+    super();
+    this.state = { isFlipped: false };
+  }
+  clickHandler = () => {
+    console.log("this is being called.");
+    this.setState({ isFlipped: !this.state.isFlipped });
+    console.log(this.state.isFlipped);
+  };
+
   render() {
+    let memoryCardInnerClass = "MemoryCardInner";
+    if (this.state.isFlipped === true) {
+      memoryCardInnerClass = "MemoryCardInner flipped";
+    }
+
     return (
-      <div className="MemoryCard">
-        <img
-          class="image"
-          src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
-          alt="Logo"
-          width="100%"
-        ></img>
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className={memoryCardInnerClass}>
+          <div className="MemoryCardBack">
+            <img
+              class="image"
+              src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
+              alt="Logo"
+              width="100%"
+            ></img>
+          </div>
+          <div className="MemoryCardFront">∆</div>
+        </div>
       </div>
     );
   }
